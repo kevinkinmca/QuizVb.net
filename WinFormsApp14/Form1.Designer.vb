fa1b5c91d95cb51057d8039ea6cff92e173f00ca
kevink.inmca2126 <kevink.inmca2126@OPENSOURCELAB24>
2024-05-17 11:16:25 +0530

    Add project files.

---
+++ WinFormsApp14/Form1.Designer.vb
@@ -0,0 +1,541 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        Label2 = New Label()
+        Timer1 = New Timer(components)
+        Label3 = New Label()
+        RadioButton5 = New RadioButton()
+        RadioButton6 = New RadioButton()
+        RadioButton7 = New RadioButton()
+        RadioButton8 = New RadioButton()
+        Label4 = New Label()
+        RadioButton9 = New RadioButton()
+        RadioButton10 = New RadioButton()
+        RadioButton11 = New RadioButton()
+        GroupBox1 = New GroupBox()
+        Button1 = New Button()
+        GroupBox2 = New GroupBox()
+        Button2 = New Button()
+        GroupBox3 = New GroupBox()
+        Button3 = New Button()
+        RadioButton12 = New RadioButton()
+        GroupBox4 = New GroupBox()
+        Button4 = New Button()
+        RadioButton16 = New RadioButton()
+        RadioButton15 = New RadioButton()
+        RadioButton14 = New RadioButton()
+        RadioButton13 = New RadioButton()
+        Label5 = New Label()
+        GroupBox5 = New GroupBox()
+        Button5 = New Button()
+        RadioButton20 = New RadioButton()
+        RadioButton19 = New RadioButton()
+        RadioButton18 = New RadioButton()
+        RadioButton17 = New RadioButton()
+        Label6 = New Label()
+        Button6 = New Button()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        GroupBox4.SuspendLayout()
+        GroupBox5.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(359, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(6, 54)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(97, 19)
+        RadioButton1.TabIndex = 1
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "RadioButton1"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(119, 54)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(97, 19)
+        RadioButton2.TabIndex = 2
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "RadioButton2"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(6, 106)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(97, 19)
+        RadioButton3.TabIndex = 3
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "RadioButton3"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(119, 106)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(97, 19)
+        RadioButton4.TabIndex = 4
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "RadioButton4"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(34, 30)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 5
+        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(21, 20)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Label3"
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Location = New Point(6, 48)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(97, 19)
+        RadioButton5.TabIndex = 7
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "RadioButton5"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton6
+        ' 
+        RadioButton6.AutoSize = True
+        RadioButton6.Location = New Point(132, 48)
+        RadioButton6.Name = "RadioButton6"
+        RadioButton6.Size = New Size(97, 19)
+        RadioButton6.TabIndex = 8
+        RadioButton6.TabStop = True
+        RadioButton6.Text = "RadioButton6"
+        RadioButton6.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton7
+        ' 
+        RadioButton7.AutoSize = True
+        RadioButton7.Location = New Point(6, 100)
+        RadioButton7.Name = "RadioButton7"
+        RadioButton7.Size = New Size(97, 19)
+        RadioButton7.TabIndex = 9
+        RadioButton7.TabStop = True
+        RadioButton7.Text = "RadioButton7"
+        RadioButton7.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton8
+        ' 
+        RadioButton8.AutoSize = True
+        RadioButton8.Location = New Point(132, 100)
+        RadioButton8.Name = "RadioButton8"
+        RadioButton8.Size = New Size(97, 19)
+        RadioButton8.TabIndex = 10
+        RadioButton8.TabStop = True
+        RadioButton8.Text = "RadioButton8"
+        RadioButton8.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(16, 33)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(41, 15)
+        Label4.TabIndex = 11
+        Label4.Text = "Label4"
+        ' 
+        ' RadioButton9
+        ' 
+        RadioButton9.AutoSize = True
+        RadioButton9.Location = New Point(6, 63)
+        RadioButton9.Name = "RadioButton9"
+        RadioButton9.Size = New Size(97, 19)
+        RadioButton9.TabIndex = 12
+        RadioButton9.TabStop = True
+        RadioButton9.Text = "RadioButton9"
+        RadioButton9.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton10
+        ' 
+        RadioButton10.AutoSize = True
+        RadioButton10.Location = New Point(126, 63)
+        RadioButton10.Name = "RadioButton10"
+        RadioButton10.Size = New Size(103, 19)
+        RadioButton10.TabIndex = 13
+        RadioButton10.TabStop = True
+        RadioButton10.Text = "RadioButton10"
+        RadioButton10.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton11
+        ' 
+        RadioButton11.AutoSize = True
+        RadioButton11.Location = New Point(0, 102)
+        RadioButton11.Name = "RadioButton11"
+        RadioButton11.Size = New Size(103, 19)
+        RadioButton11.TabIndex = 14
+        RadioButton11.TabStop = True
+        RadioButton11.Text = "RadioButton11"
+        RadioButton11.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton4)
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Location = New Point(82, 55)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(253, 162)
+        GroupBox1.TabIndex = 15
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(76, 133)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 6
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Button2)
+        GroupBox2.Controls.Add(Label3)
+        GroupBox2.Controls.Add(RadioButton5)
+        GroupBox2.Controls.Add(RadioButton6)
+        GroupBox2.Controls.Add(RadioButton7)
+        GroupBox2.Controls.Add(RadioButton8)
+        GroupBox2.Location = New Point(467, 61)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(247, 156)
+        GroupBox2.TabIndex = 16
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "GroupBox2"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(85, 127)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 11
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(Button3)
+        GroupBox3.Controls.Add(RadioButton12)
+        GroupBox3.Controls.Add(Label4)
+        GroupBox3.Controls.Add(RadioButton9)
+        GroupBox3.Controls.Add(RadioButton10)
+        GroupBox3.Controls.Add(RadioButton11)
+        GroupBox3.Location = New Point(90, 264)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(245, 150)
+        GroupBox3.TabIndex = 17
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "GroupBox3"
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(79, 127)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 16
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton12
+        ' 
+        RadioButton12.AutoSize = True
+        RadioButton12.Location = New Point(126, 102)
+        RadioButton12.Name = "RadioButton12"
+        RadioButton12.Size = New Size(103, 19)
+        RadioButton12.TabIndex = 15
+        RadioButton12.TabStop = True
+        RadioButton12.Text = "RadioButton12"
+        RadioButton12.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox4
+        ' 
+        GroupBox4.Controls.Add(Button4)
+        GroupBox4.Controls.Add(RadioButton16)
+        GroupBox4.Controls.Add(RadioButton15)
+        GroupBox4.Controls.Add(RadioButton14)
+        GroupBox4.Controls.Add(RadioButton13)
+        GroupBox4.Controls.Add(Label5)
+        GroupBox4.Location = New Point(467, 264)
+        GroupBox4.Name = "GroupBox4"
+        GroupBox4.Size = New Size(247, 150)
+        GroupBox4.TabIndex = 18
+        GroupBox4.TabStop = False
+        GroupBox4.Text = "GroupBox4"
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(85, 127)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 5
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton16
+        ' 
+        RadioButton16.AutoSize = True
+        RadioButton16.Location = New Point(138, 102)
+        RadioButton16.Name = "RadioButton16"
+        RadioButton16.Size = New Size(103, 19)
+        RadioButton16.TabIndex = 4
+        RadioButton16.TabStop = True
+        RadioButton16.Text = "RadioButton16"
+        RadioButton16.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton15
+        ' 
+        RadioButton15.AutoSize = True
+        RadioButton15.Location = New Point(6, 102)
+        RadioButton15.Name = "RadioButton15"
+        RadioButton15.Size = New Size(103, 19)
+        RadioButton15.TabIndex = 3
+        RadioButton15.TabStop = True
+        RadioButton15.Text = "RadioButton15"
+        RadioButton15.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton14
+        ' 
+        RadioButton14.AutoSize = True
+        RadioButton14.Location = New Point(138, 63)
+        RadioButton14.Name = "RadioButton14"
+        RadioButton14.Size = New Size(103, 19)
+        RadioButton14.TabIndex = 2
+        RadioButton14.TabStop = True
+        RadioButton14.Text = "RadioButton14"
+        RadioButton14.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton13
+        ' 
+        RadioButton13.AutoSize = True
+        RadioButton13.Location = New Point(6, 63)
+        RadioButton13.Name = "RadioButton13"
+        RadioButton13.Size = New Size(103, 19)
+        RadioButton13.TabIndex = 1
+        RadioButton13.TabStop = True
+        RadioButton13.Text = "RadioButton13"
+        RadioButton13.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(21, 33)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(41, 15)
+        Label5.TabIndex = 0
+        Label5.Text = "Label5"
+        ' 
+        ' GroupBox5
+        ' 
+        GroupBox5.Controls.Add(Button5)
+        GroupBox5.Controls.Add(RadioButton20)
+        GroupBox5.Controls.Add(RadioButton19)
+        GroupBox5.Controls.Add(RadioButton18)
+        GroupBox5.Controls.Add(RadioButton17)
+        GroupBox5.Controls.Add(Label6)
+        GroupBox5.Location = New Point(264, 420)
+        GroupBox5.Name = "GroupBox5"
+        GroupBox5.Size = New Size(258, 144)
+        GroupBox5.TabIndex = 19
+        GroupBox5.TabStop = False
+        GroupBox5.Text = "GroupBox5"
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(78, 120)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 5
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton20
+        ' 
+        RadioButton20.AutoSize = True
+        RadioButton20.Location = New Point(134, 95)
+        RadioButton20.Name = "RadioButton20"
+        RadioButton20.Size = New Size(103, 19)
+        RadioButton20.TabIndex = 4
+        RadioButton20.TabStop = True
+        RadioButton20.Text = "RadioButton20"
+        RadioButton20.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton19
+        ' 
+        RadioButton19.AutoSize = True
+        RadioButton19.Location = New Point(12, 95)
+        RadioButton19.Name = "RadioButton19"
+        RadioButton19.Size = New Size(103, 19)
+        RadioButton19.TabIndex = 3
+        RadioButton19.TabStop = True
+        RadioButton19.Text = "RadioButton19"
+        RadioButton19.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton18
+        ' 
+        RadioButton18.AutoSize = True
+        RadioButton18.Location = New Point(134, 57)
+        RadioButton18.Name = "RadioButton18"
+        RadioButton18.Size = New Size(103, 19)
+        RadioButton18.TabIndex = 2
+        RadioButton18.TabStop = True
+        RadioButton18.Text = "RadioButton18"
+        RadioButton18.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton17
+        ' 
+        RadioButton17.AutoSize = True
+        RadioButton17.Location = New Point(12, 57)
+        RadioButton17.Name = "RadioButton17"
+        RadioButton17.Size = New Size(103, 19)
+        RadioButton17.TabIndex = 1
+        RadioButton17.TabStop = True
+        RadioButton17.Text = "RadioButton17"
+        RadioButton17.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(30, 28)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(41, 15)
+        Label6.TabIndex = 0
+        Label6.Text = "Label6"
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(348, 33)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 23)
+        Button6.TabIndex = 20
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 591)
+        Controls.Add(Button6)
+        Controls.Add(GroupBox5)
+        Controls.Add(GroupBox4)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        GroupBox4.ResumeLayout(False)
+        GroupBox4.PerformLayout()
+        GroupBox5.ResumeLayout(False)
+        GroupBox5.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RadioButton9 As RadioButton
+    Friend WithEvents RadioButton10 As RadioButton
+    Friend WithEvents RadioButton11 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents RadioButton12 As RadioButton
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents RadioButton16 As RadioButton
+    Friend WithEvents RadioButton15 As RadioButton
+    Friend WithEvents RadioButton14 As RadioButton
+    Friend WithEvents RadioButton13 As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents RadioButton20 As RadioButton
+    Friend WithEvents RadioButton19 As RadioButton
+    Friend WithEvents RadioButton18 As RadioButton
+    Friend WithEvents RadioButton17 As RadioButton
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button6 As Button
+
+End Class
---
+++ WinFormsApp14/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "QUIZ"
+        GroupBox1.Text = "Question:1"
+        GroupBox2.Text = "Question:2"
+        GroupBox3.Text = "Question:3"
+        GroupBox4.Text = "Question:4"
+        GroupBox5.Text = "Question:5"
+        GroupBox1.Visible = False
+        GroupBox2.Visible = False
+        GroupBox3.Visible = False
+        GroupBox4.Visible = False
+        GroupBox5.Visible = False
+        Button1.Text = "Next"
+        Button2.Text = "Next"
+        Button3.Text = "Next"
+        Button4.Text = "Next"
+        Button5.Text = "Finish"
+        Button6.Text = "START"
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        GroupBox1.Visible = True
+        Label2.Text = "What is the capital of France?"
+        RadioButton1.Text = "A) Berlin"
+        RadioButton2.Text = "B) Madrid"
+        RadioButton3.Text = "C) Paris"
+        RadioButton4.Text = "D) Rome"
+        Button6.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GroupBox2.Visible = True
+        Label3.Text = "Which planet is known as the Red Planet?"
+        RadioButton5.Text = "A) Venus "
+        RadioButton6.Text = "B) Mars "
+        RadioButton7.Text = "C) Jupiter "
+        RadioButton8.Text = "D) Saturn"
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        GroupBox3.Visible = True
+        Label4.Text = "Who wrote the play ‘Romeo and Juliet’?"
+        RadioButton9.Text = "A) William Shakespeare"
+        RadioButton10.Text = "B) Charles Dickens"
+        RadioButton11.Text = "C) Jane Austen"
+        RadioButton12.Text = "D)  George Orwell"
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        GroupBox4.Visible = True
+        Label5.Text = "What is the largest mammal in the world?"
+        RadioButton13.Text = "A) African Elephant"
+        RadioButton14.Text = "B) Blue Whale"
+        RadioButton15.Text = "C) Giraffe"
+        RadioButton16.Text = "D) Polar Bear"
+        Button3.Enabled = False
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        GroupBox5.Visible = True
+        Label6.Text = "Which element has the chemical symbol ‘O’?"
+        RadioButton17.Text = "A) Gold"
+        RadioButton18.Text = "B) Oxygen"
+        RadioButton19.Text = "C) Iron"
+        RadioButton20.Text = "D) Helium"
+        Button4.Enabled = False
+    End Sub
+End Class
